fe2d9fbaf5e7f6bfaa2fd8ba3cf707782efce52c
YCEP <YCEP@sit.nyp.edu.sg>
2023-06-15 09:14:20 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# # no_of_apples = 5
+# # message = "my shopping basket has"
+# # print(message, no_of_apples)
+# #
+# # thislist = ["apple" , "banana" , "cherry" ]
+# # thislist[1] = "blackcurrant"
+# # print(thislist)
+# #
+# # thislist = ["apple" , "banana" , "cherry" ]
+# # thislist.append("orange")
+# # print(thislist)
+# # thislist.append("ahmad")
+# # print(thislist)
+# #
+# # thislist = ["apple" , "banana" , "cherry" ]
+# # thislist.pop(1)
+# # print(thislist)
+# #
+# # if 5 > 2: print("5 is greater than 2")
+# # if 2 < 5: print("2 is not greater than 5")
+#
+# # time = 1800
+# # if time > 1000 and time < 2259:
+# #     print('you no need to sleep')
+# # elif time > 2300:
+# #     print('go sleep')
+#
+# rows = int(input("How many rows do you want?"))
+# for i in range(1, rows+1):
+#         for j in range(1,i+1):
+#             print(j, end="")
+#         print("")A
+
